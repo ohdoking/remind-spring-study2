57642787de9a8a81b54c297effe00c47bec444f3
ohdoking <ohdoking@gmail.com>
2018-02-01 20:42:59 +0900

    project init

---
+++ src/main/java/com/ohdoking/remind/RemindSpringApplication.java
@@ -0,0 +1,12 @@
+package com.ohdoking.remind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindSpringApplication.class, args);
+	}
+}
